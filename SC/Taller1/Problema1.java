81143dfed6af03c5c54a5f1b03e9c8b9b530785f
Carlos Domonte Lapto <CharlieDomonte>
2019-05-30 19:58:34 -0500

    Test

---
+++ SC/Taller1/Problema1.java
@@ -0,0 +1,19 @@
+package Problema_1
+import java.util.Scanner;
+{
+public class Taller 1
+
+        String name;
+        Scanner Keyboard = new Scanner(System.in);
+
+public void show(){
+        System.out.println("Bienvenido a mi proyecto #1");
+        System.out.println("Ingrese su nombre: ");
+        name=Keyboard.next();
+
+        System.out.Printin("Su nombre es: "+nombre);
+
+        }
+
+        }
+        }
